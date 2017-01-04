bd7ead7f5965b2e295b79596bbf1af7d38ae5cb7
ysterp <ysterp@users.noreply.github.com>
2017-01-04 16:21:45 -0500

    Update player-interaction.js

--- src/player-interaction.js
+++ src/player-interaction.js
@@ -13,7 +13,7 @@ class PlayerInteraction {
   //
   // Returns an {Observable} that will `onNext` for each player that joins and
   // `onCompleted` when time expires or the max number of players join.
-  static pollPotentialPlayers(messages, channel, scheduler=rx.Scheduler.timeout, timeout=10, maxPlayers=10) {
+  static pollPotentialPlayers(messages, channel, scheduler=rx.Scheduler.timeout, timeout=20, maxPlayers=10) {
     let formatMessage = t => `Who wants to play? Respond with 'yes' in this channel in the next ${t} seconds.`;
     let timeExpired = PlayerInteraction.postMessageWithTimeout(channel, formatMessage, scheduler, timeout);
 
